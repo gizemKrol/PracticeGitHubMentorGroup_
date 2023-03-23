655ff21a4f089e25b0ae62dd649e3c77cd90ccf7
Gizem Krolikiewicz <gizyuz1201@gmail.com>
2023-03-23 14:37:48 -0400

    Initial commit

---
+++ src/main/java/Practice01/PracticeClass01.java
@@ -0,0 +1,9 @@
+package Practice01;
+
+import java.sql.SQLOutput;
+
+public class PracticeClass01 {
+    public static void main(String[] args) {
+        System.out.println("hello guys");
+    }
+}

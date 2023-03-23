68534be543310e1bed835d6748bb9aaca3d9d2da
Gizem Krolikiewicz <gizyuz1201@gmail.com>
2023-03-23 14:40:30 -0400

    first commit

--- src/main/java/Practice01/PracticeClass01.java
+++ src/main/java/Practice01/PracticeClass01.java
@@ -5,5 +5,6 @@ import java.sql.SQLOutput;
 public class PracticeClass01 {
     public static void main(String[] args) {
         System.out.println("hello guys");
+        System.out.println("denem yaptık");
     }
 }
